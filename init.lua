59342905d87dcc1d5a6995b9ce334fd4f5295742
phamhiep2506 <phamhiep25062k@gmail.com>
2024-03-27 02:59:27 +0700

    nvim: add debug tool c#

--- init.lua
+++ init.lua
@@ -270,5 +270,33 @@ require("lazy").setup({
         end
       })
     end
+  },
+  {
+    -- Debug
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio"
+    },
+    config = function()
+      require("dapui").setup()
+      local dap = require("dap")
+      -- C#
+      dap.adapters.coreclr = {
+        type = "executable",
+        command = vim.fn.stdpath("data") .. "/mason/packages/netcoredbg/netcoredbg/netcoredbg",
+        args = { "--interpreter=vscode" }
+      }
+      dap.configurations.cs = {
+        {
+          type = "coreclr",
+          name = "launch - netcoredbg",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+          end
+        }
+      }
+    end
   }
 })
